e528a5e84109352c8e591ee877a0c1c7b39ab346
ThomasFaria <thomas.faria@ensae.fr>
2024-09-16 12:19:36 +0000

    remove "next/head"

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,21 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "cratt",
   description: "<3",
+  icons: {
+    icon: "/favicon-32x32.png", // Define the favicon here
+  },
 };
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon-32x32.png"/>
-      </Head>
       <body className={inter.className}>
-            {children}
+        {children}
       </body>
     </html>
   );
